d64f58173ca07b0c1dd83a773ec25075a74eeccd
Austin <105258719+Austin-540@users.noreply.github.com>
2025-07-26 15:56:31 +1200

    made it so you can died

--- kiwijam-game/Assets/EnemyScript.cs
+++ kiwijam-game/Assets/EnemyScript.cs
@@ -7,7 +7,7 @@ public class EnemyScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,7 +19,18 @@ public class EnemyScript : MonoBehaviour
         {
             Vector2 direction = (player.transform.position - transform.position).normalized;
             transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
-        }   
-        
+        }
+
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //destroy the player and the enemy
+            Destroy(other.gameObject); // Destroy the player
+            Destroy(gameObject); // Destroy the enemy
+            Debug.Log("Player hit by enemy! Game Over.");
+        }
     }
 }
