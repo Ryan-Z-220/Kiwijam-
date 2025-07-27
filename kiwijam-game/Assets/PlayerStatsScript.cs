862d4a2eb53ec0245fc7a9bf9f5399622c8718a9
Matthew Tao <matthew.yingtao@gmail.com>
2025-07-27 17:37:07 +1200

    Fix fire rate calculation and update build settings

--- kiwijam-game/Assets/PlayerStatsScript.cs
+++ kiwijam-game/Assets/PlayerStatsScript.cs
@@ -51,7 +51,7 @@ public class PlayerStatsScript : MonoBehaviour
             }
         }
 
-        PlayerBulletScript.fireRate = 1f * firingRateMultiplier;
+        PlayerBulletScript.fireRate = 1f / firingRateMultiplier;
         PlayerBulletScript.bulletSpeed = 5f * bulletSpeedMultiplier;
 
         PlayerController2D.speed = 3f * movementSpeedMultiplier;
