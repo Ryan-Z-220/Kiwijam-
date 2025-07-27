39e25a986261ad654e18974aefe28055e2cc8792
Austin <105258719+Austin-540@users.noreply.github.com>
2025-07-27 14:46:51 +1200

    update intro comic

---
+++ kiwijam-game/Assets/StartGameButtonScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class StartGameButtonScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void buttonPressed()
+    {
+        // Load the main game scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+}
